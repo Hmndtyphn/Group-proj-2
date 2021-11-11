7a340a55dcc0dba7fbeb8fcb00c4608f095e7420
HmndTyphn <Jared.elliott09@gmail.com>
2021-11-10 19:52:30 -0600

    server.js dependencies

---
+++ server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+
+const exphbs = require('express-handlebars');
+
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
