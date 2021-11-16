b3ebf96ecc3f3bb59d206f97b6a0ec12d689f6b2
Doug Peterson <douglaspeterson6@gmail.com>
2021-11-15 20:48:34 -0600

    added home-routes.js

---
+++ controllers/home-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {User, Favorites} = require('../models');
+
+router.get('/', (req, res) => {
+    User.findAll({
+        attribute: [
+            'id',
+            'username',
+            'email',
+            'password'
+        ],
+        include: [
+            {
+                model: Favorites,
+                attributes: ['id', 'fav_url', 'created_at']
+            }
+        ]
+    }).then(dbData => {
+        const favorites = dbData.map(favorites => favorites.get({plain: true}));
+
+        res.render('login');
+    });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
-
+router.use('/', homeRoutes);
 router.use((req, res) => {
     res.status(404).end();
   });
