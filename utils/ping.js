39f90fe70ab7a5150078873e9e8b06aed9ff77c3
Doug Peterson <douglaspeterson6@gmail.com>
2021-11-21 21:12:18 -0600

    fix heroku?

--- utils/ping.js
+++ utils/ping.js
@@ -1,15 +1,13 @@
 const ping = require('ping');
+const tcpp = require('tcp-ping-node');
 
 const pingList = async hosts => {
     let result = hosts
     for(i = 0; i < hosts.length; i++){
-        let res = await ping.promise.probe(hosts[i].fav_url, {
-            numeric: true,
-            min_reply: 1,
-            extra: ['-i', '2']
-        });
+        let res = await tcpp.ping({ host: hosts[i].fav_url, timeout: 10000});
+        console.log(res);
         // await resultPush(res);
-        result[i].dataValues.favStatus = res.alive
+       result[i].dataValues.favStatus = res.success
     };
     // returnList(result)
     return result
